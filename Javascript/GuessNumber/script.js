27c6cd975dc69d8eeb5529eb290ad255bdc56d36
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-02-05 14:41:42 +0300

    end the guessgame version

--- Javascript/GuessNumber/script.js
+++ Javascript/GuessNumber/script.js
@@ -1,1 +1,57 @@
-'use strict';
+"use strict";
+/*
+console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "Correct Number";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+document.querySelector(".guess").value = 23;
+document.querySelector(".guess").value;
+constant is immutabel
+*/
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "there is no number";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number";
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "red";
+    document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".highscore").textContent = score;
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      score = score - 1;
+      document.querySelector(".message").textContent = "guss lower!";
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You Lost 😭";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      score = score - 1;
+      document.querySelector(".message").textContent = "guss higher! ";
+
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You Lost 😭";
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".message").textContent = "Start Gussing";
+  document.querySelector(".score").textContent = score;
+  document.querySelector("body").style.backgroundColor = "black";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+});
