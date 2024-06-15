68b274427564d7c4f4a66259e9519bd29f0eefcb
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-06-15 12:39:35 +0300

    explory the world of let,const,var

--- Javascript/fundamental1/script.js
+++ Javascript/fundamental1/script.js
@@ -1,3 +1,26 @@
 // start javascript 6/13/2024
 
 console.log("hello javascript");
+
+console.log(0 + 1);
+console.log(0 + "0");
+console.log(typeof "0");
+console.log(0 == 0);
+console.log(0 > "1");
+
+let js = "amazing";
+
+// if (js === "amazing") alert("Javascript is Fun");
+console.log(js);
+
+console.log(typeof "abenezer");
+
+/* what is value is the smaliest information of  what wehave
+    varibles is they hold or assign a value in memeore or they are memory refresnes
+
+
+*/
+
+let name = "bob";
+
+console.log(name);
