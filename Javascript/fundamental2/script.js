b55afadd4de90b94d0106d0fdb9cf2b0e40aa699
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-06-22 12:43:42 +0300

    start f2

---
+++ Javascript/fundamental2/script.js
@@ -0,0 +1,19 @@
+// Start sat,22 june
+"use strict";
+
+let hasDriverLicense = false;
+let passTest = true;
+
+if (passTest) {
+  passTest = false;
+}
+
+/* functions im js */
+
+function logger() {
+  console.log("i comming from function");
+}
+
+logger(); // invock caling runnig
+logger();
+logger();
