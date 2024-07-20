b806f0429f2ce7102810761242b418a5591b5fec
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-07-20 20:30:11 +0300

    DOM

--- Javascript/gusseNumber/script.js
+++ Javascript/gusseNumber/script.js
@@ -1,11 +1,15 @@
 "use strict";
 
-console.log(document.querySelector(".message").textContent);
+// console.log(document.querySelector(".message").textContent);
 
-document.querySelector(".message").textContent = "correct number";
-document.querySelector(".number").textContent = 19;
-document.querySelector(".score").textContent = 18;
-const guess = (document.querySelector(".guess").value = Math.trunc(
-  Math.random() * 21 + 1
-));
-console.log(guess);
+// document.querySelector(".message").textContent = "correct number";
+// document.querySelector(".number").textContent = 19;
+// document.querySelector(".score").textContent = 18;
+// const guess = (document.querySelector(".guess").value = Math.trunc(
+//   Math.random() * 21 + 1
+// ));
+// console.log(guess);
+
+document.querySelector(".submit").addEventListener("click", function () {
+  console.log(document.querySelector(".guess").value);
+});
